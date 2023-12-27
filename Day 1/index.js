bbe3a56aae2b5ad5542473fd3f2473d6fe7eaa79
beqabur1 <148256504+beqabur1@users.noreply.github.com>
2023-12-27 19:59:03 +0400

    Work

---
+++ Day 1/index.js
@@ -0,0 +1,9 @@
+//variables
+
+let userName = 'lasha'; //string
+let usareAge = 19; //number
+let userStatus = false; //booLean true =1; false = 0
+let x = null; // null
+let y; //undefined
+console.log(typeof userName);
+console.log(typeof x)
