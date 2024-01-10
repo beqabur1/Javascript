f9f4af27f89ab09579ede3a64d5d48d5d8cd0bec
beqabur1 <148256504+beqabur1@users.noreply.github.com>
2024-01-10 21:12:28 +0400

    No

--- კლ;ასგარეშე/index.js
+++ კლ;ასგარეშე/index.js
@@ -1,26 +1,14 @@
-async function getWeather() {
-    const city = document.getElementById('cityInput').value;
-    const apiKey = 'YOUR_API_KEY'; // Replace with your OpenWeatherMap API key
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  
-    try {
-      const response = await fetch(apiUrl);
-      const data = await response.json();
-  
-      const weatherInfo = document.getElementById('weatherInfo');
-      if (data.cod === '404') {
-        weatherInfo.textContent = 'City not found';
-      } else {
-        const cityName = data.name;
-        const temperature = data.main.temp;
-        const weatherDescription = data.weather[0].description;
-  
-        weatherInfo.innerHTML = `<h2>${cityName}</h2>
-                                 <p>Temperature: ${temperature}°C</p>
-                                 <p>Description: ${weatherDescription}</p>`;
-      }
-    } catch (error) {
-      console.log('Error:', error);
-    }
-  }
-  
+function  updateClock() {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, '0');
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  const seconds = now.getSeconds().padStart(2, '0');
+
+  const timeElement = document.getElementById('time');
+  timeElement.textContent = '$(hours):$(minutes):$(seconds)';
+}
+// Update the clock every second
+setInterval(updateClock, 1000);
+
+//initial call to displey the clock immediately
+updateClock()
