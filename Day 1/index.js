f59d5e74f2e9cc22f7cb814044a09af9b2055096
beqabur1 <148256504+beqabur1@users.noreply.github.com>
2024-01-13 13:24:41 +0400

    Day 1

--- Day 1/index.js
+++ Day 1/index.js
@@ -1,9 +1,32 @@
-//variables
+let x = 100;
+if (x < 50){
+    console.log("less than 50");
+} else if (x > 20){
+    console.log("greater than 20");
+}else {
+    console.log("error");
+}
 
-let userName = 'lasha'; //string
-let usareAge = 19; //number
-let userStatus = false; //booLean true =1; false = 0
-let x = null; // null
-let y; //undefined
-console.log(typeof userName);
-console.log(typeof x)
+let name2 = "mariam"
+if (name2 === "mariam"){
+    console.log(true);
+} else{
+    console.log(false);
+}
+
+let name1 = name === "mariam" ? true : false
+
+switch (name){
+    case "mariam":
+        console.log(true);
+        break;
+    default:
+        console.log(false);
+}
+
+let num = Number(prompt("ENTER NUMBER"));
+if (num % 2 === 0) {
+    console.log("even number");
+} else {
+    console.log("odd number");
+}
