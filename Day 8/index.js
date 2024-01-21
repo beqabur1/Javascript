d3f7de5b464a62e4c2f1224d6a412194b4b05de9
beqabur1 <148256504+beqabur1@users.noreply.github.com>
2024-01-21 15:55:24 +0400

    ძლივს

--- Day 8/index.js
+++ Day 8/index.js
@@ -1,4 +1,8 @@
-document.getElementById('burger-icon').addEventListener('click', function() {
-    document.getElementById('menu-list').classList.toggle('show');
-  });
-  
+const hamMenu = document.querySelector(".ham-menu");
+
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
